c67d3c8d85b7de052ccb2ffabc94271e1cc3e73b
CMX\vgevorgyan <vgevorgyan@cinimex.ru>
2022-05-28 15:15:34 +0300

    Create React Learning Project

---
+++ index.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+<h1>Hello everyone!</h1> ,
+ document.getElementById("root")
+ );
+
+ const nav = (
+    <nav>
+     <h1>website</h1>
+         <ul>
+             <li>Pricing</li>
+             <li>About</li>
+             <li>Contact</li>
+         </ul>
+ </nav>
+)
+ReactDOM.render(nav, document.getElementById("root"))

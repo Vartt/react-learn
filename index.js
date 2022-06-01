1aae85bc0bd3137c4acc2a29f8813a55d08334f3
CMX\vgevorgyan <vgevorgyan@cinimex.ru>
2022-06-01 18:26:19 +0300

    move `header` element from Page

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
+            <Header />
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
